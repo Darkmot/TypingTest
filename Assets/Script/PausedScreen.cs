da215ce117d4d12716682e7e722e5b60a760e221
Timotius Prihadi <thedarkmot@gmail.com>
2017-03-14 06:45:00 +0700

    Change Credit & Settings to Paused Screen

---
+++ Assets/Script/PausedScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausedScreen : BaseScreen {
+
+    GameObject previousScreen;
+
+    public void SetPreviousScreen(GameObject ps)
+    {
+        previousScreen = ps;
+    }
+
+    public void ShowPreviousScreen()
+    {
+        if (previousScreen != null)
+        {
+            previousScreen.GetComponent<BaseScreen>().ActivateScreen(true);
+            previousScreen = null;
+        }
+    }
+}
