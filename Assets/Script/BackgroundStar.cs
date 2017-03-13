2e5288c8f3d1e060a41cfa83c08370c87e83ffdc
Timotius Prihadi <thedarkmot@gmail.com>
2017-03-13 14:55:28 +0700

    Credit Screen

---
+++ Assets/Script/BackgroundStar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundStar : MonoBehaviour {
+    
+    public int speed = 100;
+    public int limitY = 400;
+
+    RectTransform rt;
+    void Start()
+    {
+        rt = (RectTransform)gameObject.transform;
+    }
+
+	void Update () {
+        float newY = rt.anchoredPosition.y - ((float)speed*Time.deltaTime);
+        if (newY < -limitY)
+            newY += limitY;
+        Vector2 newPos = new Vector2(rt.anchoredPosition.x,newY);
+        rt.anchoredPosition = newPos;
+	}
+}
---
+++ Assets/Script/BaseScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseScreen : MonoBehaviour {
+
+    public CanvasGroup cg;
+
+    public void ActivateScreen(bool show)
+    {
+        gameObject.SetActive(true);
+        if (show)
+        {
+            cg.alpha = 0f;
+            StartCoroutine (Fade(0.05f));
+        }
+        else
+        {
+            cg.alpha = 1f;
+            StartCoroutine(Fade(-0.05f));
+        }        
+    }
+    IEnumerator Fade(float delta)
+    {
+        if (delta < 0)
+        {
+            while (cg.alpha > 0f)
+            {
+                cg.alpha += delta;
+                yield return null;
+            }
+            cg.alpha = 0f;
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            while (cg.alpha < 1f)
+            {
+                cg.alpha += delta;
+                yield return null;
+            }
+            cg.alpha = 1f;
+        }
+
+    }
+
+}
