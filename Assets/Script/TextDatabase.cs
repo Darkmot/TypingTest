31177931487e41bfca143589b6f629d0f3ea287d
Timotius Prihadi <thedarkmot@gmail.com>
2017-03-14 08:32:00 +0700

    Text Database

---
+++ Assets/Script/TextDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDatabase : MonoBehaviour {
+
+    public TextAsset dictionaryFile;
+    private List<List<string>> line;
+	void Start () {
+        string wholeText = dictionaryFile.text;
+        List<string> eachLine = new List<string>();
+        eachLine.AddRange(wholeText.Split("\n"[0]));
+
+        int curLength = 0;
+        line = new List<List<string>>();
+        line.Add(new List<string>());
+
+        foreach (string newLine in eachLine)
+        {
+            while (newLine.Length > curLength)
+            {
+                line.Add(new List<string>());
+                curLength++;
+            }
+            line[curLength].Add(newLine);
+        }
+
+    }
+
+    public string GetRandomWord(int wordLength)
+    {
+        if ((wordLength > line.Count + 1) || (line[wordLength].Count == 0))
+        {
+            return null;
+        }
+        else
+        {
+            return line[wordLength][Random.Range(0, line[wordLength].Count)];
+        }
+    }
+}
